696faa9eae7dcc147256bc2c5b517bb23a21df1b
Vladimir Dabić <dabic.vladimir@gmail.com>
2020-12-10 01:20:26 +0100

    day 6 - part 2

--- src/day6-custom-customs/custom-customs.test.ts
+++ src/day6-custom-customs/custom-customs.test.ts
@@ -1,7 +1,12 @@
 import chalk from "chalk";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fixture = require("./fixture.json") as string[];
-import { sumCollectedAnswers, countAnswersForGroup } from "./custom-customs";
+import {
+  sumCollectedAnswers,
+  sumCollectedAnswersV2,
+  countAnswersForGroup,
+  countAnswersForGroupV2,
+} from "./custom-customs";
 
 const exampleFixture = [
   ["abc"],
@@ -11,7 +16,7 @@ const exampleFixture = [
   ["b"],
 ];
 
-describe("day 6 - custom customs", () => {
+describe("day 6 - custom customs - part 1", () => {
   it.each`
     group                   | expected
     ${["abc"]}              | ${3}
@@ -23,14 +28,41 @@ describe("day 6 - custom customs", () => {
     const result = countAnswersForGroup(group);
     expect(result).toBe(expected);
   });
-  it.only("should work for example fixture", () => {
+  it("should work for example fixture", () => {
     const result = sumCollectedAnswers(exampleFixture);
     console.log({ result });
     expect(result).toBe(11);
   });
-  it.only("should give me the result", () => {
+  it("should give me the result", () => {
     const result = sumCollectedAnswers(fixture);
     expect(result).toBe(6686);
+    console.log(
+      chalk.blue(`
+        day 6 - custom customs = ${chalk.yellow(result)}
+        `)
+    );
+  });
+});
+describe("day 6 - custom customs - part 2", () => {
+  it.each`
+    group                   | expected
+    ${["abc"]}              | ${3}
+    ${["a", "b", "c"]}      | ${0}
+    ${["ab", "ac"]}         | ${1}
+    ${["a", "a", "a", "a"]} | ${1}
+    ${["b"]}                | ${1}
+  `("group $group should count $expected", ({ group, expected }) => {
+    const result = countAnswersForGroupV2(group);
+    expect(result).toBe(expected);
+  });
+  it("should work for example fixture", () => {
+    const result = sumCollectedAnswersV2(exampleFixture);
+    console.log({ result });
+    expect(result).toBe(6);
+  });
+  it("should give me the result", () => {
+    const result = sumCollectedAnswersV2(fixture);
+    expect(result).toBe(3476);
     console.log(
       chalk.blue(`
         day 6 - custom customs = ${chalk.yellow(result)}
--- src/day6-custom-customs/custom-customs.ts
+++ src/day6-custom-customs/custom-customs.ts
@@ -1,7 +1,19 @@
+import _ from "lodash";
+
 export const countAnswersForGroup = (group: string[]) =>
   [...new Set(group.join("").split(""))].length;
 
 export const sumCollectedAnswers = (answers: any[]) =>
   answers.reduce((acc, curr) => {
     return acc + countAnswersForGroup(curr);
+  }, 0);
+
+/* part 2 */
+
+export const countAnswersForGroupV2 = (group: string[]) =>
+  _.intersection(...group.map(i => i.split(""))).length;
+
+export const sumCollectedAnswersV2 = (answers: any[]) =>
+  answers.reduce((acc, curr) => {
+    return acc + countAnswersForGroupV2(curr);
   }, 0);
